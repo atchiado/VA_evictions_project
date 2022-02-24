f80820c55ffd8cb805255d6993495cdd0f7fa5a3
Anthony Chiado <anthonychiado97@gmail.com>
2022-02-24 13:52:01 -0500

    Imputing data to script

--- VA_cities_evictions_heatmap.R
+++ VA_cities_evictions_heatmap.R
@@ -1,1 +1,9 @@
+## setup
+library(plyr)
 library(tidyverse)
+library(reshape)
+library(scales)
+
+## import data
+city_evictions = read.csv(file = "~/Desktop/work/data/r/VA_evictions_project/cities.csv")
+county_evictions = read.csv(file = "~/Desktop/work/data/r/VA_evictions_project/counties.csv")
